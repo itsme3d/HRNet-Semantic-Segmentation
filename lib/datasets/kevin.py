7ba9a7159b9816ed6b028db56700516bcdf240fe
Kevin Branigan <kbranigan@gmail.com>
2019-09-20 16:00:54 -0400

    label mapping fun

--- lib/datasets/kevin.py
+++ lib/datasets/kevin.py
@@ -34,6 +34,32 @@ class Kevin(BaseDataset):
         super(Kevin, self).__init__(ignore_label, base_size,
                 crop_size, downsample_rate, scale_factor, mean, std)
 
+        # self.label_mapping = {-1: ignore_label, 0: ignore_label, 
+        #                       1: ignore_label, 2: ignore_label, 
+        #                       3: ignore_label, 4: ignore_label, 
+        #                       5: ignore_label, 6: ignore_label, 
+        #                       7: 0, 8: 1, 9: ignore_label, 
+        #                       10: ignore_label, 11: 2, 12: 3, 
+        #                       13: 4, 14: ignore_label, 15: ignore_label, 
+        #                       16: ignore_label, 17: 5, 18: ignore_label, 
+        #                       19: 6, 20: 7, 21: 8, 22: 9, 23: 10, 24: 11,
+        #                       25: 12, 26: 13, 27: 14, 28: 15, 
+        #                       29: ignore_label, 30: ignore_label, 
+        #                       31: 16, 32: 17, 33: 18}
+                              
+        self.label_mapping = {-1: -1, 0: 0, 
+                              1: 1, 2: 2, 
+                              3: 3, 4: 4, 
+                              5: 5, 6: 6, 
+                              7: 7, 8: 8, 9: 9, 
+                              10: 10, 11: 11, 12: 12, 
+                              13: 13, 14: 14, 15: 15, 
+                              16: 16, 17: 17, 18: 18, 
+                              19: 19, 20: 20, 21: 21, 22: 22, 23: 23, 24: 24,
+                              25: 25, 26: 26, 27: 27, 28: 28, 
+                              29: 29, 30: 30, 
+                              31: 31, 32: 32, 33: 33}
+
         self.root = root
         self.num_classes = num_classes
         self.list_path = list_path
@@ -177,11 +203,11 @@ class Kevin(BaseDataset):
 
     def convert_label(self, label, inverse=False):
         temp = label.copy()
-        # if inverse:
-        #     for v, k in self.label_mapping.items():
-        #         label[temp == k] = v
-        # else:
-        #     for k, v in self.label_mapping.items():
-        #         label[temp == k] = v
+        if inverse:
+            for v, k in self.label_mapping.items():
+                label[temp == k] = v
+        else:
+            for k, v in self.label_mapping.items():
+                label[temp == k] = v
         return label
     
